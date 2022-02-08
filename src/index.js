7e653b0a692ef3430d3a25c4173cb25a841566c5
gogystrezh21 <gogystrezh21@gmail.com>
2022-02-08 19:13:18 +0100

    init:add js

--- src/index.js
+++ src/index.js
@@ -36,11 +36,31 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
 function decode(expr) {
-    // write your solution here
-}
+	let decodedText = '';
 
+	for (let i = 0; i < expr.length; i = i + 10) {
+		const letterSpace = expr.slice(i, i + 10);
+		let letterMorse = '';
+		if (letterSpace== '**********') {
+			decodedText += ' ';
+			continue;
+		}
+		for (let j = 9; j > 0; j = j - 2) {
+			const morseSign = letterSpace[j - 1] + letterSpace[j];
+			if (morseSign == '00') {
+				break;
+			} else if (morseSign == '10') {
+				letterMorse = `.${letterMorse}`;
+			} else if (morseSign == '11') {
+				letterMorse = `-${letterMorse}`;
+			}
+		}
+		decodedText = decodedText + MORSE_TABLE[letterMorse];
+	}
+	return decodedText;
+}
+	
 module.exports = {
     decode
 }
